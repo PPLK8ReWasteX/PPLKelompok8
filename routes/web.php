73a0389b2266fac9b7928989c547d0ddfd1c9238
Najmaqurrotulaeni <najma0603@gmail.com>
2025-04-11 11:04:01 +0700

    PPL8-13 PBI-003A: Menampilkan Daftar artikel

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EcoNewsController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/econews', [NewsController::class, 'index'])->name('econews');
 Route::get('/econews/category/{id}', [EcoNewsController::class, 'filterByCategory'])->name('econews.filter.category');
 Route::get('/econews/tag/{id}', [EcoNewsController::class, 'filterByTag'])->name('econews.filter.tag');
-Route::get('/econews/{id}', [App\Http\Controllers\NewsController::class, 'show'])->name('econews.detail');
+Route::get('/econews/{id}', [NewsController::class, 'show'])->name('econews.detail');
 
-});
+// Jangan ada kurung tutup di sini!
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [UserController::class, 'profile'])->name('profile');
@@ -19,3 +22,4 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/profile/update', [UserController::class, 'updateProfile'])->name('profile.update');
     Route::post('/profile/upload-picture', [UserController::class, 'uploadProfilePicture'])->name('profile.uploadPicture');
     Route::post('/profile/deactivate', [UserController::class, 'deactivateAccount'])->name('profile.deactivate');
+});
